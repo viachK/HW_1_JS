8983671d59bb54e4574e6eb565b6cbde3b848338
Viacheslav Kovryzhnykh <106442178+viachK@users.noreply.github.com>
2022-06-19 17:08:28 +0200

    Add files via upload

---
+++ lesson_1/Lesson_1_JS_HW_1_Ex_3.js
@@ -0,0 +1,29 @@
+// 3**:
+// Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+let age_2 = 18
+let age_3 = 60
+
+const checkAge = function (age) {
+
+    age_1 = Number(age)
+    if (!isNaN(age)) {
+
+        if (age < age_2) {
+            console.log("You don’t have access cause your age is " + age + ". It’s less than ")
+        }
+        else if (age >= age_2 && age < age_3) {
+            console.log("Welcome !")
+        }
+        else if
+            (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+        } else {
+            console.log("Technical work")
+        }
+    } else {
+        console.log("Error")
+    }
+}
+
+checkAge('12')
